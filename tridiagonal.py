b01853b61c506f98f68e7f2495432f94304d5dac
onur sahin <onursahin@onurs-MacBook-Air.local>
2026-03-26 02:57:10 +0300

    hydrus source code

--- tridiagonal.py
+++ tridiagonal.py
@@ -12,24 +12,6 @@ root_model_type = RootWaterUptake.class_type.instance_type
 spec = {"soil_model":soil_model_type,"root_model":root_model_type,'top_boundary':int32,'bot_boundary':int32,'dz':int32,'N':int32,"ha":float64,'hs':float64,
         "A":float64[:],"B":float64[:],"C":float64[:],"F":float64[:]} # defining the tridiagonals
 
-@njit
-def solve_thomas(A,B,C,F):
-    n = B.shape[0]
-    alfa,beta,y,h_new = np.zeros(n),np.zeros(n),np.zeros(n),np.zeros(n)
-    alfa[0] = B[0]
-    beta[0] = (C[0] / alfa[0])
-    y[0] = F[0] / alfa[0]
-    for i in range(1,n):
-        alfa[i] = B[i] - A[i - 1] * beta[i - 1]
-        beta[i] = C[i] / alfa[i]
-        y[i] = (F[i] - y[i - 1] * A[i - 1]) / alfa[i]
-    h_new[n - 1] = y[n - 1]
-    for j in range(1,n):
-        r = int(n) - 1 - j
-        h_new[r] = y[r] - beta[r] *  h_new[r + 1]
-    return h_new
-
-
 @jitclass(spec)
 class CreateTriDiagonal:
     def __init__(self,soil_model,root_model,dz,top_bound,bot_bound):
@@ -49,16 +31,16 @@ class CreateTriDiagonal:
        
         if self.top_bound == 4: # atmospheric boundary
             if (self.head[self.n_1] < self.ha) and (flux_top>=0): # lower boundary
-                self.B[self.n_1] =1; self.A[self.n_1] = 0; self.F[self.n_1] = 0
+                self.B[self.n_1] =1; self.A[self.n_2] = 0; self.F[self.n_1] = 0
                 self.head[self.n_1] = self.ha
 
             if (head_top[self.n_1]>=0): # upper boundary handling surface layer if ponding_max > 0 also
-                self.B[self.n_1] =1; self.A[self.n_1] = 0; self.F[self.n_1] = 0
+                self.B[self.n_1] =1; self.A[self.n_2] = 0; self.F[self.n_1] = 0
                 self.head[self.n_1] = pond
 
         else:
-            if (self.top_bound  == 0) or (self.top_bound == 2): # constant or variable head boundary (main function should change time series of heads)
-                self.B[self.n_1] =1; self.A[self.n_1] = 0; self.F[self.n_1] = 0
+            if (self.top_bound  == 0) or (self.top_bound == 2): # constant or variable head boundary!
+                self.B[self.n_1] =1; self.A[self.n_2] = 0; self.F[self.n_1] = 0
                 self.head[self.n_1] = head_top
             else: # constant or variable flux boundary condition!
                 k11 = (self.k[self.n_1] + self.k[self.n_2]) / 2
@@ -96,7 +78,6 @@ class CreateTriDiagonal:
         self.head[:] = h2[:]
         self.root_model.calculate_sink_source(self.head,tp)
         sink = self.root_model.sink  
-        dz2 = np.power(self.dz, 2) 
         self.set_top(dt,flux_top,head_top,pond,sink)
         self.set_bot(dt,flux_bot,head_bot,sink)
         self.soil_model.calculate_props(h1,self.head)
@@ -105,11 +86,27 @@ class CreateTriDiagonal:
         for i in range(1,self.n-1):
             k1 = (self.k[i] + self.k[i - 1]) / 2
             k2 = (self.k[i] + self.k[i + 1]) / 2
-            self.A[i-1] = -k1 / dz2
-            self.B[i] = ((k1 + k2) / dz2 + self.cap[i] / dt)
-            self.C[i] = -k2 / dz2
-            self.F[i] = (self.s1[i] - self.s2[i]) / dt + k2 * (self.head[i + 1] - self.head[i]) / dz2 - k1 * (
-                        self.head[i] - self.head[i - 1]) / dz2+ (k2 - k1) / self.dz - sink[i]
+            self.A[i-1] = -k1 / self.dz2
+            self.B[i] = ((k1 + k2) / self.dz2 + self.cap[i] / dt)
+            self.C[i] = -k2 / self.dz2
+            self.F[i] = (self.s1[i] - self.s2[i]) / dt + k2 * (self.head[i + 1] - self.head[i]) / self.dz2 - k1 * (
+                        self.head[i] - self.head[i - 1]) / self.dz2+ (k2 - k1) / self.dz - sink[i]
         
         return solve_thomas(self.A,self.B,self.C,self.F) + self.head
         
+@njit
+def solve_thomas(A,B,C,F):
+    n = B.shape[0]
+    alfa,beta,y,h_new = np.zeros(n),np.zeros(n),np.zeros(n),np.zeros(n)
+    alfa[0] = B[0]
+    beta[0] = (C[0] / alfa[0])
+    y[0] = F[0] / alfa[0]
+    for i in range(1,n):
+        alfa[i] = B[i] - A[i - 1] * beta[i - 1]
+        beta[i] = C[i] / alfa[i]
+        y[i] = (F[i] - y[i - 1] * A[i - 1]) / alfa[i]
+    h_new[n - 1] = y[n - 1]
+    for j in range(1,n):
+        r = int(n) - 1 - j
+        h_new[r] = y[r] - beta[r] *  h_new[r + 1]
+    return h_new
